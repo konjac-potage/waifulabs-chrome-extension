4082bd4cee023f0335a163a7b8600818c5ab3628
konjac_potage <code@konjac.tokyo>
2019-08-19 02:27:12 +0900

    add seek step button at final page

--- src/content_scripts/modules/api.js
+++ src/content_scripts/modules/api.js
@@ -21,19 +21,19 @@ export async function generate_big(seeds) {
 }
 
 /**
- * fetch big image
+ * fetch next girls
  * @param {(number|number[])[]} seeds
+ * @param {number} step
  * @return {Promise<string>} base64-encoded string content of image/png
  */
-export async function generate(seeds) {
-  const res = await fetch('https://api.waifulabs.com/generate_big', {
+export async function generate(seeds, step) {
+  const res = await fetch('https://api.waifulabs.com/generate', {
     body: JSON.stringify({
       currentGirl: seeds,
-      size: 512,
-      step: 4,
+      step,
     }),
     method: 'POST',
   });
 
-  return (await res.json()).girl;
+  return (await res.json()).newGirls;
 }
--- src/content_scripts/seek-step.js
+++ src/content_scripts/seek-step.js
@@ -3,12 +3,16 @@
  */
 
 import App from './modules/app';
+import * as API from './modules/api';
+import { getReactInstance } from './modules/utils';
 
 /**
  * @param {App} app
  */
 export function applyPatch(app) {
   const step = app.getStep();
+
+  // page with step bar
   if (0 <= step && step <= 3) {
     const targets = [
       ...document.querySelectorAll('.ant-steps-item-content'),
@@ -24,5 +28,35 @@ export function applyPatch(app) {
         dom.style.cursor = null;
       }
     });
+  }
+
+  // final step page
+  else if (step == 4) {
+    const parent = document.querySelector('.my-girl-container');
+    if (parent && !parent['_register_seek_step']) {
+      parent['_register_seek_step'] = true; // apply patch only once
+
+      ['Color', 'Details', 'Pose'].forEach((name, index) => {
+        // create button to seek step
+        const button = parent
+          .appendChild(document.createElement('div'))
+          .appendChild(document.createElement('button'));
+
+        button.className = 'bp3-button bp3-fill bp3-minimal';
+        button.textContent = `Change ${name}`;
+        button.onclick = async () => {
+          // do seek by executing undo()
+          const app = new App();
+          const state = app.getState();
+
+          state.lastStep = index + 1;
+          state.lastGirl = state.girl;
+          state.lastGirls = await API.generate(state.girl.seeds, index + 1);
+          state.loading = false;
+
+          getReactInstance(parent).return.memoizedProps.undo();
+        };
+      });
+    }
   }
 }
